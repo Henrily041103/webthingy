c52184e3064f4e32e3637ed09438f4dbc0198dfb
Anh Monk <anhmonkdv@gmail.com>
2022-12-08 18:09:24 +0700

    Pre-deploy

--- src/Body/body.js
+++ src/Body/body.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import Main_Page from './Main_Page/main_page';
-import Opening_Slots from './Opening_Slots/Opening_Slots';
+import MainPage from './MainPage/MainPage';
+import OpeningSlots from './OpeningSlots/OpeningSlots';
 import Teachers from './Teachers/Teachers';
-import EStudent from "./ExceptionalStudents/EStudent"
-import StudentM from './StudentMoments/StudentM';
+// import EStudent from "./ExceptionalStudents/EStudent"
+// import StudentM from './StudentMoments/StudentM';
 import Contacts from './Contacts/Contacts';
+// import Events from './Events/Events';
 import "./body.css"
 import fb from "../assets/images/fb_button.png"
-import zalo from "../assets/images/zalo_button.png"
+// import zalo from "../assets/images/zalo_button.png"
 import call from "../assets/images/call_button.png"
 
-class Page_Body extends React.Component{
+class PageBody extends React.Component{
     render(){
         return (
             <main>
-                <Main_Page></Main_Page>
-                <Opening_Slots></Opening_Slots>
+                <MainPage></MainPage>
+                <OpeningSlots></OpeningSlots>
                 <Teachers></Teachers>
-                <StudentM></StudentM>
-                <EStudent></EStudent>
                 <Contacts></Contacts>
                 <p id = 'button_box'>
-                    <a className='SocMed' href='#'><img src = {fb} alt ="Contact Facebook"></img></a>
-                    <a className='SocMed' href='#'><img src = {zalo} alt ="Contact Zalo"></img></a>
+                    <a className='SocMed' href='https://www.facebook.com/ieltsscholar.center'><img src = {fb} alt ="Contact Facebook"></img></a>
+                    {/* <a className='SocMed' href='#'><img src = {zalo} alt ="Contact Zalo"></img></a> */}
                     <button className='SocMed' onClick={openOptions}><img src = {call} alt ="Call Us"></img></button>
                     <span id='callOptionsBox'>
                         <a className='callOptions' href="tel:+84966 13 15 14">Gọi điện thoại cho trung tâm </a>
@@ -42,4 +41,4 @@ function openOptions() {
     else box.style.display = "block";
 }
 
-export default Page_Body;
+export default PageBody;
